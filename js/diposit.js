284522f8b06268c1145d7a69ca693dea1db64998
phlabib <ph.labib@gmail.com>
2023-08-15 18:25:41 +0600

    fix some bugs

--- js/diposit.js
+++ js/diposit.js
@@ -1,6 +1,7 @@
 // step 1: add event listner to the diposite button
-document.getElementById('btn-diposit').addEventListener('click', function(){
-    // get the diposite amount from the diposite input field
+    document.getElementById('btn-diposit').addEventListener('click', function(){
+
+    // step 2 :get the diposite amount from the diposite input field
     // for input field use .value to the value inside the input field
     const dipositeField = document.getElementById('diposit-field');
     const newDipositeAmountString = dipositeField.value;
@@ -11,6 +12,7 @@ document.getElementById('btn-diposit').addEventListener('click', function(){
     const dipositTotalElement = document.getElementById('diposit-total');
     const previuosDipositTotalString = dipositTotalElement.innerText;
     const previuosDipositTotal = parseFloat(previuosDipositTotalString);
+    
     // stpe 4: add numbers to set the total diposite
     const currentDipositeTotal = previuosDipositTotal + newDipositeAmount; 
     //  set the diposite total 
--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
 // step : 1 [add click event handler with the submit button]
 document.getElementById('btn-submit').addEventListener('click', function(){
+
 // step 2 : get the email address inside the email input field
 // remember : always to use .value to get text form an input field
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
+    
 // step 3 : get password 
 // -> get the element
 //  -> get the value from the element
